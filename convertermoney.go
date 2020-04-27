2487538d5c5b16dac339770f9532ab696fbede4c
hadyd <anr.hadyd@gmail.com>
2020-04-27 17:46:17 +0700

    First Init

---
+++ convertermoney.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	inputNilaiDollar := 5.0
+	nilaiRupiah := 15528.00
+
+	dollarToRupiah := inputNilaiDollar * nilaiRupiah
+
+	fmt.Println("$", inputNilaiDollar, "=", dollarToRupiah, "Rupiah")
+}
